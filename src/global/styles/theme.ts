908faebd369eae567cf687c8269b2afdb63d1962
Natã Hermes <nhermesdev@gmail.com>
2023-01-17 12:00:45 -0800

    feat: add global styles

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,15 @@
+export default {
+    colors: {
+        primary: '#3D2F49',
+        secondary: '#FC7939',
+        white: '#FFF',
+        grey: '#C8BED1',
+        text: '#FFF',
+        title: '#FFF',
+        transparent: 'transparent',
+    },
+    fonts: {
+        medium: 'Poppins_500Medium',
+        bold: 'Poppins_700Bold',
+    }
+}
